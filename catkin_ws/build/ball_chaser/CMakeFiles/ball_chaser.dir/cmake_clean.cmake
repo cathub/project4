9db8106868ec1c16ae523b8880b915268cbdab4b
root <boli.ufl@gmail.com>
2020-10-07 01:45:41 +0000

    Initial setup.

---
+++ catkin_ws/build/ball_chaser/CMakeFiles/ball_chaser.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ball_chaser.dir/src/drive_bot.cpp.o"
+  "ball_chaser.pdb"
+  "ball_chaser"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ball_chaser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
